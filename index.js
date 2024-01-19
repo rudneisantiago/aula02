262843f94c9310fc74cd852dd97965beb65fd2c2
Rudnei Santiago <rudnei.santiago@gmail.com>
2024-01-18 21:19:56 -0300

    Commit inicial

---
+++ index.js
@@ -0,0 +1,26 @@
+// // Essa parte do código está doida
+
+// /**
+//  * Aqui fiz a função que gera um texto
+//  */
+
+// console.log("texto");
+// console.log();
+
+// function teste() {
+//   let variavel = "função";
+//   console.log(variavel);
+// }
+
+// teste();
+
+// let variavel = 1123
+// console.log(variavel);
+
+var numero = prompt("digite um número");
+
+var dez = 10;
+
+console.log(parseInt(numero) + dez);
+console.log(parseFloat(numero) + dez);
+console.log(Number(numero) + dez);
